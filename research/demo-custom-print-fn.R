7e330a126426c281efc64fbae29e5f6d1432f80c
Dereck Mezquita <dereck@mezquita.io>
2024-08-17 22:26:50 -0500

    Format msg function.

---
+++ research/demo-custom-print-fn.R
@@ -0,0 +1,16 @@
+box::use(./R/Logger[ Logger, LogLevel ])
+box::use(./R/messageParallel[ messageParallel ])
+box::use(./R/collapse[ collapse ])
+
+logger <- Logger$new(
+    level = LogLevel$INFO,
+    file_path = "logs.log",
+    print_fn = function(x) {
+        messageParallel(x)
+    },
+    format_fn = function(level, msg) {
+        paste0("Yeet ", msg)
+    }
+)
+
+logger$info("This is an info message")
